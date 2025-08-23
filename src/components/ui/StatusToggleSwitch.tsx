c4d0aff68fccedf7224cebdeb03ab48fc5f9c640
Denis M. Melo <denisdmm@gmail.com>
2025-08-23 01:01:34 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <StatusToggleSwitc

--- src/components/ui/StatusToggleSwitch.tsx
+++ src/components/ui/StatusToggleSwitch.tsx
@@ -21,19 +21,19 @@ export const StatusToggleSwitch = ({ status, onToggle }: StatusToggleSwitchProps
       onClick={onToggle}
       className={cn(
         "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2",
-        isEnabled ? "bg-white" : "bg-gray-300 dark:bg-gray-600"
+        isEnabled ? "bg-green-600" : "bg-gray-300 dark:bg-gray-600"
       )}
     >
       <span className="sr-only">Use to toggle user status</span>
       <span
         aria-hidden="true"
         className={cn(
-          "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out flex items-center justify-center",
-          isEnabled ? "translate-x-5" : "translate-x-0"
+          "pointer-events-none inline-block h-5 w-5 transform rounded-full shadow ring-0 transition duration-200 ease-in-out flex items-center justify-center",
+          isEnabled ? "translate-x-5 bg-green-500" : "translate-x-0 bg-white"
         )}
       >
         {isEnabled ? (
-          <Lock className="h-3 w-3 text-gray-700" />
+          <Lock className="h-3 w-3 text-white" />
         ) : (
           <Unlock className="h-3 w-3 text-gray-700" />
         )}
